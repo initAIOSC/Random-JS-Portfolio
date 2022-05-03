26c4aa86686fb169c7d3787c1b6af53e3ec625f5
root <alanolvera_2000@hotmail.com>
2022-05-02 20:41:11 -0500

    Aagregamos modo production, development y watch

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,26 +2,25 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
+  mode: 'development',
+  watch: true,
   resolve: {
     extensions: [".js"],
     alias: {
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@templates': path.resolve(__dirname, 'src/templates/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@images': path.resolve(__dirname, 'src/assets/images/'),
-    }
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -37,13 +36,13 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
       },
       {
-        test: /\.png/, 
-        type: 'asset/resource'
+        test: /\.png/,
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 10000,
             mimetype: "application/font-woff",
@@ -52,8 +51,8 @@ module.exports = {
             publicPath: "../assets/fonts/",
             esModule: false,
           },
-        }
-      }
+        },
+      },
     ],
   },
   plugins: [
@@ -63,7 +62,7 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css'
+      filename: "assets/[name].[contenthash].css",
     }),
     new CopyPlugin({
       patterns: [
@@ -74,13 +73,5 @@ module.exports = {
       ],
     }),
     new Dotenv(),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin(),
-    ]
-  }
 };
